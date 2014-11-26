42b23dd02a8b7e4d672ff9655ac8872af7f8f36e
Steven Gliebe <work@stevengliebe.com>
2014-11-26 13:16:41 -0600

    Add add_page method (and various code clean up)

--- ct-options.js
+++ ct-options.js
@@ -4,47 +4,47 @@
  * This is loaded in WordPress admin when the CT Options class is used.
  */
 
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function( $ ) {
 
 	// Activate section specified in URL hash
 	// or first tab if no hash in URL or if hash is invalid
 	// or first tab if just restored defaults
 	var section = location.hash.replace('#', '');
-	if (!section || !$("#ctoptions-tabs .nav-tab[data-section^='" + section + "']").length || $('#setting-error-restore_defaults').length) { // use first if none specified or if section is wrong
-		section = $("#ctoptions-tabs .nav-tab:first-child").attr('data-section');
+	if (!section || !$("#cto-tabs .nav-tab[data-section^='" + section + "']").length || $('#setting-error-restore_defaults').length) { // use first if none specified or if section is wrong
+		section = $("#cto-tabs .nav-tab:first-child").attr('data-section');
 	}
-	ctoptions_switch_section(section);
-	$('#ctoptions-tabs, #ctoptions-form').fadeIn(); // now show tabs + form after initial setup
-	
+	cto_switch_section(section);
+	$('#cto-tabs, #cto-form').fadeIn(); // now show tabs + form after initial setup
+
 	// Change tab/section on click
-	$('#ctoptions-tabs .nav-tab').click(function() {
+	$('#cto-tabs .nav-tab').click(function() {
 		var section = $(this).attr('data-section');
-		ctoptions_switch_section(section);
+		cto_switch_section(section);
 		jQuery('.settings-error').remove(); // hide message
-	});	
+	});
 
 	// Move checkbox fields not having a name into same row/cell as one having name above them
-	$('.ctoptions-checkbox').each(function() {
+	$('.cto-checkbox').each(function() {
 
 		var row = $(this).parents('tr');
 
 		// Does this field NOT have a name?
 		if ($('th', row).html() == '') { // th holds name
-			
+
 			// Does immediately preceding row contain checkbox AND have name?
 			var prev_row = row.prev('tr');
-			if ($('td .ctoptions-checkbox', prev_row).length && $('th', prev_row).html() != '') {
-			
+			if ($('td .cto-checkbox', prev_row).length && $('th', prev_row).html() != '') {
+
 				// Move this checkbox field to bottom of that row
 				$('td', prev_row).append('<div>' + $('td', row).html());
-			
+
 				// Remove old row
 				row.remove();
-				
+
 			}
 
-		}				
-	
+		}
+
 	})
 
 });
@@ -53,22 +53,22 @@ jQuery(document).ready(function($) {
  * Switch Tab/Section
  */
 
-function ctoptions_switch_section(section) {
+function cto_switch_section(section) {
 
 	// Activate tab
-	jQuery('#ctoptions-tabs .nav-tab').removeClass('nav-tab-active');
-	jQuery("#ctoptions-tabs .nav-tab[data-section^='" + section + "']").addClass('nav-tab-active');
+	jQuery('#cto-tabs .nav-tab').removeClass('nav-tab-active');
+	jQuery("#cto-tabs .nav-tab[data-section^='" + section + "']").addClass('nav-tab-active');
 
 	// Show settings
-	jQuery('#ctoptions-form tr').hide(); // hide others
-	jQuery('#ctoptions-form tr').each(function() {
-		if (jQuery('.ctoptions-section-' + section, this).length) { // show row if it has setting belonging to section
+	jQuery('#cto-form tr').hide(); // hide others
+	jQuery('#cto-form tr').each(function() {
+		if (jQuery('.cto-section-' + section, this).length) { // show row if it has setting belonging to section
 			jQuery(this).fadeIn();
-		}	
+		}
 	});
-	
+
 	// Add hash
-	jQuery('#ctoptions-form').attr('action', 'options.php#' + section); // add to <form> post so tab stays same
+	jQuery('#cto-form').attr('action', 'options.php#' + section); // add to <form> post so tab stays same
 	window.location.hash = section; // always show hash (such as first load w/none)
 
 }
--- ct-options.php
+++ ct-options.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * churchthemes.com Options Class
+ * CT Options Class
  *
- * This makes it easy to setup tabbed option pages for themes and plugins.
+ * This makes it easy to setup tabbed option pages for plugins.
  */
 
 if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and plugin
@@ -12,20 +12,23 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 		function __construct( $config ) {
 
 			// Version - used in cache busting
-			$this->version = '0.6.1'; // April 30, 2013
+			$this->version = '0.7';
 
 			// Config
 			$this->config = $config;
-			
+
 			// Prepare fields
 			$this->prepare_fields();
-			
+
+			// Add page
+			add_action( 'admin_menu', array( &$this, 'add_page' ) );
+
 			// Add fields
 			add_action( 'admin_init', array( &$this, 'add_fields' ) );
-			
+
 			// Enqueue styles
 			add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue_styles' ) );
-			
+
 			// Enqueue scripts
 			add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue_scripts' ) );
 
@@ -35,16 +38,17 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 		 * Prepare Fields
 		 *
 		 * Convert options config into simple key => value array, without sections
+		 *
+		 * @since 0.6
 		 */
-		 
 		function prepare_fields() {
-		
+
 			$this->fields = array();
-		
+
 			// Add fields from config
 			$sections = $this->config['sections'];
 			foreach( $sections as $section_key => $section ) {
-			
+
 				// Loop options in section
 				if ( ! empty( $section['fields'] ) ) {
 					foreach( $section['fields'] as $field_id => $field_config ) {
@@ -53,17 +57,36 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 						$this->fields[$field_id] = $field_config;
 					}
 				}
-				
+
 			}
-			
+
+		}
+
+		/**
+		 * Add Page
+		 *
+		 * @since 0.7
+		 */
+		function add_page() {
+
+			// Add options page to menu
+			add_options_page(
+				$this->config['page_title'],		// text shown in window title
+				$this->config['menu_title'], 		// text shown in menu
+				'manage_options', 					// role/capability with access
+				$this->config['menu_slug'],			// unique menu/page slug
+				array( &$this, 'page_content' )		// callback providing output for page
+			);
+
 		}
 
 		/**
 		 * Add Fields
+		 *
+		 * @since 0.6
 		 */
-
 		function add_fields() {
-			
+
 			// Add section for all options on the page
 			add_settings_section(
 				$this->config['option_id'], 			// section ID (using same as master option ID)
@@ -71,10 +94,10 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 				array( &$this, 'settings_content' ),	// callback that produces output above setting fields
 				$this->config['menu_slug'] 				// menu page
 			);
-			
+
 			// Add fields from config
 			foreach( $this->fields as $id => $field ) {
-			
+
 				add_settings_field(
 					$id,
 					! empty( $field['name'] ) ? $field['name'] : '',
@@ -86,122 +109,126 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 						$field
 					)
 				);
-			
+
 			}
-		
+
 			// Register the fields
 			register_setting(
 				$this->config['option_id'],
 				$this->config['option_id'],
 				array( &$this, 'sanitize' ) // callback for sanitization before saving
-			);		
-			
+			);
+
 		}
-		
+
 		/**
 		 * Enqueue Stylesheets
 		 *
 		 * Load stylesheets only when option page is being shown.
+		 *
+		 * @since 0.6
 		 */
-		
 		function enqueue_styles() {
 
 			// Don't load where not needed
 			if ( $this->is_options_page() ) {
 
-				wp_enqueue_style( 'ct-options', trailingslashit( CTOPTIONS_URL ) . 'ct-options.css', false, $this->version ); // bust cache on update
+				wp_enqueue_style( 'ct-options', trailingslashit( CTO_URL ) . 'ct-options.css', false, $this->version ); // bust cache on update
 
 			}
-			
+
 		}
-		
+
 		/**
 		 * Enqueue Scripts
 		 *
 		 * Load scripts only when option page is being shown.
+		 *
+		 * @since 0.6
 		 */
-		
 		function enqueue_scripts() {
 
 			// Don't load where not needed
 			if ( $this->is_options_page() ) {
-			
-				wp_enqueue_script( 'ct-options', trailingslashit( CTOPTIONS_URL ) . 'ct-options.js', false, $this->version ); // bust cache on update
+
+				wp_enqueue_script( 'ct-options', trailingslashit( CTO_URL ) . 'ct-options.js', false, $this->version ); // bust cache on update
 
 			}
-			
+
 		}
-		
+
 		/**
 		 * Page Content
 		 *
 		 * This displays the option page tabs and fields.
+		 *
+		 * @since 0.6
 		 */
-
 		function page_content() {
 
 			// Output contents
 			?>
-			<div id="ctoptions-content" class="wrap">
+			<div id="cto-content" class="wrap">
 
-				<?php screen_icon(); ?>
-				
-				<h2><?php esc_html_e( $this->config['page_title'] ); ?></h2>
-				
+				<h2><?php echo esc_html( $this->config['page_title'] ); ?></h2>
+
 				<?php settings_errors(); ?>
-				
-				<h2 id="ctoptions-tabs" class="nav-tab-wrapper">
+
+				<h2 id="cto-tabs" class="nav-tab-wrapper">
 					<?php foreach( $this->config['sections'] as $slug => $section ) : ?>
 					<a href="#<?php echo esc_attr( $slug ); ?>" data-section="<?php echo esc_attr( $slug ); ?>" class="nav-tab"><?php echo esc_html( $section['title'] ); ?></a>
 					<?php endforeach; ?>
 				</h2>
 
-				<form id="ctoptions-form" method="post" action="options.php">
-				
+				<form id="cto-form" method="post" action="options.php">
+
 					<?php
 					settings_fields( $this->config['option_id'] );
 					do_settings_sections( $this->config['menu_slug'] );
 					submit_button();
 					?>
-					
-					<input type="submit" class="button-secondary" name="restore_defaults" value="<?php _e( 'Restore Defaults', 'church-theme' ); ?>" onclick="return confirm('<?php echo esc_js( __( 'Are you sure you want to restore default values for settings in all tabs?', 'church-theme' ) ); ?>');" />
-				
+
 				</form>
-				
+
 			</div>
 			<?php
 
 		}
-		
+
 		/**
 		 * Settings Content
 		 *
 		 * Output will appear above the option fields.
+		 *
+		 * @since 0.6
 		 */
+		function settings_content() {
 
-		function settings_content() {		
-			// nothing
+			// Nothing
+
 		}
-		
+
 		/**
 		 * Field Content
 		 *
 		 * Render output for a field based on its type as specified in $field from config
+		 *
+		 * @since 0.6
+		 * @param array $args Field arguments
 		 */
-
 		function field_content( $args ) {
-		
+
 			$data = array();
-		
+
 			// Get field config from arguments
 			$data['id'] = $args[0];
 			$data['field'] = $args[1];
-			
+
 			// Prepare strings
 			$data['value'] = $this->get( $data['id'] );
 			$data['esc_value'] = esc_attr( $this->get( $data['id'] ) );
-			$data['esc_element_id'] = 'ctoptions-field-' . esc_attr( $data['id'] );
-			
+			$data['esc_element_id'] = 'cto-field-' . esc_attr( $data['id'] );
+
 			// Prepare styles for elements (core WP styling)
 			$default_classes = array(
 				'text'		=> 'regular-text',
@@ -212,7 +239,7 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 				'number'	=> 'small-text'
 			);
 			$classes = array();
-			$classes[] = 'ctoptions-' . $data['field']['type'];
+			$classes[] = 'cto-' . $data['field']['type'];
 			if ( ! empty( $default_classes[$data['field']['type']] ) ) {
 				$classes[] = $default_classes[$data['field']['type']];
 			}
@@ -220,46 +247,46 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 				$classes[] = $data['field']['class'];
 			}
 			$data['classes'] = implode( ' ', $classes );
-			
+
 			// Common attributes
 			$data['common_atts'] = 'name="' . esc_attr( $this->config['option_id'] . '[' . $data['id'] . ']' ) . '" class="' . esc_attr( $data['classes'] ) . '"';
 			if ( ! empty( $data['field']['attributes'] ) ) { // add custom attributes
 				foreach( $data['field']['attributes'] as $attr_name => $attr_value ) {
 					$data['common_atts'] .= ' ' . $attr_name . '="' . esc_attr( $attr_value ) . '"';
-				}		
+				}
 			}
-		
+
 			// Use custom function to output field
 			if ( ! empty( $data['field']['custom_content'] ) ) {
 				$html = call_user_func( $data['field']['custom_content'], $args, $data );
 			}
-		
+
 			// Standard output based on type
 			else {
-			
+
 				// Switch thru types to render differently
 				$html = '';
 				switch ( $data['field']['type'] ) {
-				
+
 					// Text
 					case 'text':
-					
+
 						$html = '<input type="text" ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '" value="' . $data['esc_value'] . '" />';
-					
+
 						break;
 
 					// Textarea
 					case 'textarea':
-					
+
 						$html = '<textarea ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '">' . esc_textarea( $data['value'] ) . '</textarea>';
-						
+
 						// special esc func for textarea
-					
+
 						break;
 
 					// Checkbox
 					case 'checkbox':
-					
+
 						$html  = '<input type="hidden" ' . $data['common_atts'] . ' value="" />'; // causes unchecked box to post empty value (helps with default handling)
 						$html .= '<label for="' . $data['esc_element_id'] . '">';
 						$html .= '	<input type="checkbox" ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '" value="1"' . checked( '1', $data['value'], false ) . '/>';
@@ -267,210 +294,210 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 							$html .= ' ' . $data['field']['checkbox_label'];
 						}
 						$html .= '</label>';
-						
+
 						break;
 
 					// Radio
 					case 'radio':
-					
+
 						if ( ! empty( $data['field']['options'] ) ) {
-						
+
 							foreach( $data['field']['options'] as $option_value => $option_text ) {
-							
+
 								$esc_radio_id = $data['esc_element_id'] . '-' . $option_value;
-							
-								$html .= '<div>';				
+
+								$html .= '<div>';
 								$html .= '	<label for="' . $esc_radio_id . '">';
 								$html .= '		<input type="radio" ' . $data['common_atts'] . ' id="' . $esc_radio_id . '" value="' . esc_attr( $option_value ) . '"' . checked( $option_value, $data['value'], false ) . '/> ' . esc_html( $option_text );
 								$html .= '	</label>';
 								$html .= '</div>';
-								
+
 							}
-							
+
 						}
-					
+
 						break;
 
 					// Select
 					case 'select':
-					
+
 						if ( ! empty( $data['field']['options'] ) ) {
-						
+
 							$html .= '<select ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '">';
 							foreach( $data['field']['options'] as $option_value => $option_text ) {
 								$html .= '<option value="' . esc_attr( $option_value ) . '" ' . selected( $option_value, $data['value'], false ) . '> ' . esc_html( $option_text ) . '</option>';
 							}
 							$html .= '</select>';
-							
+
 						}
-					
+
 						break;
-				
+
 					// Number
 					case 'number':
-					
+
 						$html = '<input type="number" ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '" value="' . $data['esc_value'] . '" />';
-					
+
 						break;
 
 				}
-			
+
 			}
-			
+
 			// Add description beneath
 			if ( ! empty( $data['field']['desc'] ) ) {
 				$html .= '<p class="description">' . $data['field']['desc'] . '</p>';
 			}
-			
+
 			// Wrap field
-			$html = '<div class="ctoptions-section-' . esc_attr( $this->fields[$data['id']]['section'] ) . '"> ' . $html . '</div>';
-			
+			$html = '<div class="cto-section-' . esc_attr( $this->fields[$data['id']]['section'] ) . '"> ' . $html . '</div>';
+
 			// Output filterable
-			echo apply_filters( 'ctoptions_field_content', $html, $args );
-		
+			echo apply_filters( 'cto_field_content', $html, $args );
+
 		}
-		
+
 		/**
 		 * Sanitization
 		 *
 		 * Sanitize values before saving. Also handles restoring defaults.
+		 *
+		 * @since 0.6
+		 * @param array $input Values being saved
+		 * @return array Sanitized values
 		 */
-
 		function sanitize( $input ) {
-		
+
 			global $allowedposttags;
-						
-			// Restore Defaults clicked
-			// Wipe user input so all options get cleared
-			if ( ! empty( $_POST['restore_defaults'] ) ) {
-				$input = array();
-				add_settings_error( $this->config['option_id'], 'restore_defaults', __( 'Defaults restored.', 'church-theme' ), 'updated' );			
-			}
-			
+
 			// Define the array for the updated options
 			$output = array();
-			
+
 			// Loop values
 			foreach( $input as $key => $value ) {
-			
+
 				// Sanitization for all
 				$value = trim( stripslashes( $value ) );
-				
+
 				// Sanitize based on type
 				switch ( $this->fields[$key]['type'] ) {
-				
+
 					// Text
 					// Textarea
 					case 'text':
 					case 'textarea':
-										
+
 						// Strip tags if config does not allow HTML
 						if ( empty( $this->fields[$key]['allow_html'] ) ) {
 							$value = trim( strip_tags( $value ) );
 						}
-				
+
 						// Sanitize HTML in case used (remove evil tags like script, iframe) - same as post content
 						$value = stripslashes( wp_filter_post_kses( addslashes( $value ), $allowedposttags ) );
-						
+
 						break;
 
 					// Checkbox
 					case 'checkbox':
 
 						$value = ! empty( $value ) ? '1' : '';
-					
+
 						break;
 
 					// Radio
 					// Select
 					case 'radio':
 					case 'select':
-					
+
 						// If option invalid, stick with current value
 						if ( ! isset( $this->fields[$key]['options'][$value] ) ) {
 							$value = $this->get( $key );
 						}
-					
+
 						break;
-				
+
 					// Number
 					case 'number':
-					
+
 						$value = (int) $value; // force number
-					
+
 						break;
 
 				}
-				
+
 				// Run additional custom sanitization function if config requires it
 				if ( ! empty( $this->fields[$key]['custom_sanitize'] ) ) {
 					$value = call_user_func( $this->fields[$key]['custom_sanitize'], $value );
-				}			
-				
+				}
+
 				// Final trim
 				$value = trim( $value );
-				
+
 				// Add to output array
 				$output[$key] = $value;
-				
+
 			}
 
 			// Return clean values, make filterable
-			return apply_filters( 'ctoptions_sanitize', $output, $input );
-		
+			return apply_filters( 'cto_sanitize', $output, $input );
+
 		}
-		
+
 		/**
 		 * Is Options Page
 		 *
-		 * Are we on an options page for a theme or plugin?
+		 * Are we on the options page for the plugin?
+		 *
+		 * @since 0.6
+		 * @return bool True if on options page
 		 */
-		
 		function is_options_page() {
-		
+
 			$screen = get_current_screen();
-			
+
 			$is_options_page = false;
-			
-			// Only on an options page
+
 			if ( preg_match( '/^.*_' . $this->config['menu_slug'] . '$/', $screen->base ) ) {
 				$is_options_page = true;
 			}
-			
-			return apply_filters( 'ctoptions_is_options_page', $is_options_page );
-		
+
+			return apply_filters( 'cto_is_options_page', $is_options_page );
+
 		}
-		
+
 		/**
 		 * Get Option
 		 *
-		 * options.php should wrap this in its own option getter for more convenient use in templates, etc.
+		 * The plugin should wrap this in its own setting getter for more convenient use.
 		 * This also handles returning defaults if necessary.
+		 *
+		 * @since 0.6
+		 * @param string $option Setting slug
+		 * @return mixed Value of setting
 		 */
-		 
 		function get( $option ) {
 
 			$value = '';
-		
+
 			// Get options array to pull value from
 			$options = get_option( $this->config['option_id'] );
-			
+
 			// Get default value
 			$default = isset( $this->fields[$option]['default'] ) ? $this->fields[$option]['default'] : '';
-			
+
 			// Option not saved - use default value
 			if ( ! isset( $options[$option] ) ) {
 				$value = $default;
 			}
-			
+
 			// Option has been saved
 			else {
-				
+
 				// Value is empty when not allowed, set default (no_empty true or is radio)
 				if ( empty( $options[$option] ) && ( ! empty( $this->fields[$option]['no_empty'] ) || 'radio' == $$this->fields[$option]['type'] ) ) {
 					$value = $default;
 				}
-				
+
 				// Otherwise, stick with current value
 				else {
 					$value = $options[$option];
@@ -479,10 +506,10 @@ if ( ! class_exists( 'CT_Options' ) ) { // in case class used in both theme and 
 			}
 
 			// Return filterable
-			return apply_filters( 'ctoptions_get', $value, $option );
-			
+			return apply_filters( 'cto_get', $value, $option );
+
 		}
-		
+
 	}
-	
+
 }
