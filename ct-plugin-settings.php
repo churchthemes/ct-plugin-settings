5943fa84bf110f5f8f26aa98334abbfdd75c56c6
Steven Gliebe <3372396+stevengliebe@users.noreply.github.com>
2020-12-07 12:15:48 -0600

    🚀 RELEASE: 1.2.3

--- ct-plugin-settings.php
+++ ct-plugin-settings.php
@@ -10,18 +10,18 @@
  * https://github.com/churchthemes/church-theme-content
  *
  * @package   CT_Plugin_Settings
- * @copyright Copyright (c) 2013 - 2018, ChurchThemes.com
+ * @copyright Copyright (c) 2013 - 2020, ChurchThemes.com
  * @link      https://github.com/churchthemes/ct-plugin-settings
  * @license   GPLv2 or later
  */
 
 // No direct access.
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined( 'ABSPATH' )) {
 	exit;
 }
 
 // Class may be used in multiple plugins.
-if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both theme and plugin.
+if (! class_exists( 'CT_Plugin_Settings' )) { // in case class used in both theme and plugin.
 
 	/**
 	 * Main class.
@@ -98,7 +98,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 		public function __construct( $config ) {
 
 			// Version.
-			$this->version = '1.2.2';
+			$this->version = '1.2.3';
 
 			// Prepare data.
 			$this->prepare_data( $config );
@@ -159,7 +159,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			$this->sections = apply_filters( 'ctps_sections', $this->sections );
 
 			// Filter individual sections.
-			foreach ( $this->sections as $section_key => $section ) {
+			foreach ($this->sections as $section_key => $section) {
 				$this->sections[ $section_key ] = apply_filters( 'ctps_section-' . $section_key, $section );
 			}
 
@@ -178,7 +178,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			$this->fields = array();
 
 			// Add fields from config.
-			foreach ( $this->sections as $section_key => $section ) {
+			foreach ($this->sections as $section_key => $section) {
 
 				// Get fields for section.
 				$fields = isset( $section['fields'] ) ? (array) $section['fields'] : arrray();
@@ -188,7 +188,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 				$fields = apply_filters( 'ctps_fields-' . $section_key, $fields );
 
 				// Loop fields.
-				foreach ( $fields as $field_id => $field_config ) {
+				foreach ($fields as $field_id => $field_config) {
 
 					$field_config['id'] = $field_id; // set key as ID in field config.
 					$field_config['section'] = $section_key; // make section easily accessible.
@@ -236,7 +236,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 		public function after_save() {
 
 			// Plugin settings just saved.
-			if ( $this->is_settings_page() && ! empty( $_GET['settings-updated'] ) ) {
+			if ($this->is_settings_page() && ! empty( $_GET['settings-updated'] )) {
 				do_action( 'ctps_after_save' );
 			}
 
@@ -255,7 +255,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 		public function add_plugin_action_link( $links ) {
 
 			// Have links array?
-			if ( is_array( $links ) ) {
+			if (is_array( $links )) {
 
 				// Append "Settings" link.
 				$links[] = '<a href="' . esc_url( admin_url( 'options-general.php?page=' . $this->plugin_dir ) ) . '">' . __( 'Settings' ) . '</a>'; // use core WP 'Settings' string for this class to be plugin-agnostic.
@@ -283,17 +283,17 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			);
 
 			// Add fields from config.
-			foreach ( $this->fields as $id => $field ) {
+			foreach ($this->fields as $id => $field) {
 
 				// Name.
 				$name = '';
-				if ( ! empty( $field['name'] ) ) {
+				if (! empty( $field['name'] )) {
 
 					// Get name.
 					$name = $field['name'];
 
 					// Append text after name.
-					if ( ! empty( $field['after_name'] ) ) {
+					if (! empty( $field['after_name'] )) {
 						$name .= ' <span class="ctps-after-name">' . $field['after_name'] . '</span>';
 					}
 
@@ -354,7 +354,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 		public function enqueue_styles() {
 
 			// Don't load where not needed.
-			if ( $this->is_settings_page() ) {
+			if ($this->is_settings_page()) {
 				wp_enqueue_style( 'ct-plugin-settings', trailingslashit( $this->config['url'] ) . 'css/ct-plugin-settings.css', false, $this->version ); // bust cache on update.
 			}
 
@@ -371,7 +371,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 		public function enqueue_scripts() {
 
 			// Don't load where not needed.
-			if ( $this->is_settings_page() ) {
+			if ($this->is_settings_page()) {
 
 				// Enable media-upload and thickbox for upload fields.
 				wp_enqueue_media();
@@ -399,7 +399,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 				<h2><?php echo esc_html( $this->config['page_title'] ); ?></h2>
 
-				<?php if ( ! empty( $this->config['desc'] ) ) : ?>
+				<?php if (! empty( $this->config['desc'] )) : ?>
 
 					<p>
 						<?php
@@ -430,7 +430,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 				<h2 id="ctps-tabs" class="nav-tab-wrapper">
 
-					<?php foreach ( $this->sections as $slug => $section ) : ?>
+					<?php foreach ($this->sections as $slug => $section) : ?>
 
 						<a href="#<?php echo esc_attr( $slug ); ?>" data-section="<?php echo esc_attr( $slug ); ?>" class="nav-tab"><?php echo esc_html( $section['title'] ); ?></a>
 
@@ -463,13 +463,13 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 		 */
 		public function settings_content() {
 
-			if ( ! empty( $this->sections ) ) {
+			if (! empty( $this->sections )) {
 
 				// Output description for each section.
-				foreach ( $this->sections as $section_slug => $section ) {
+				foreach ($this->sections as $section_slug => $section) {
 
 					// JavaScript will show the desription for the active tab.
-					if ( ! empty( $section['desc'] ) ) {
+					if (! empty( $section['desc'] )) {
 						echo '<p id="ctps-section-desc-' . esc_attr( $section_slug ) . '" class="ctps-section-desc">';
 						echo wp_kses(
 							$section['desc'],
@@ -543,12 +543,12 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			$classes[] = 'ctps-' . $data['field']['type'];
 
 			// Type class.
-			if ( ! empty( $default_classes[ $data['field']['type'] ] ) ) {
+			if (! empty( $default_classes[ $data['field']['type'] ] )) {
 				$classes[] = $default_classes[ $data['field']['type'] ];
 			}
 
 			// Custom class.
-			if ( ! empty( $data['field']['class'] ) ) {
+			if (! empty( $data['field']['class'] )) {
 				$classes[] = trim( $data['field']['class'] );
 			}
 
@@ -557,9 +557,9 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 			// Common attributes.
 			$data['common_atts'] = 'name="' . esc_attr( $this->config['option_id'] . '[' . $data['id'] . ']' ) . '" class="' . esc_attr( $data['classes'] ) . '"';
-			if ( ! empty( $data['field']['attributes'] ) ) { // add custom attributes.
+			if (! empty( $data['field']['attributes'] )) { // add custom attributes.
 
-				foreach ( $data['field']['attributes'] as $attr_name => $attr_value ) {
+				foreach ($data['field']['attributes'] as $attr_name => $attr_value) {
 					$data['common_atts'] .= ' ' . $attr_name . '="' . esc_attr( $attr_value ) . '"';
 				}
 
@@ -567,12 +567,12 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 			// Inline radio inputs?
 			$inline_class = '';
-			if ( ! empty( $data['field']['inline'] ) ) {
+			if (! empty( $data['field']['inline'] )) {
 				$inline_class = ' ctps-inline';
 			}
 
 			// Use custom function to output field.
-			if ( ! empty( $data['field']['custom_content'] ) ) {
+			if (! empty( $data['field']['custom_content'] )) {
 				$html = call_user_func( $data['field']['custom_content'], $args, $data );
 			}
 
@@ -582,7 +582,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 				$html = '';
 
 				// Switch thru types to render differently.
-				switch ( $data['field']['type'] ) {
+				switch ($data['field']['type']) {
 
 					// Text.
 					case 'text':
@@ -621,7 +621,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 						$html .= '	<input type="checkbox" ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '" value="1"' . checked( '1', $data['value'], false ) . '/>';
 
-						if ( ! empty( $data['field']['checkbox_label'] ) ) {
+						if (! empty( $data['field']['checkbox_label'] )) {
 							$html .= $data['field']['checkbox_label'];
 						}
 
@@ -632,9 +632,9 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 					// Radio.
 					case 'radio':
 
-						if ( ! empty( $data['field']['options'] ) ) {
+						if (! empty( $data['field']['options'] )) {
 
-							foreach ( $data['field']['options'] as $option_value => $option_text ) {
+							foreach ($data['field']['options'] as $option_value => $option_text) {
 
 								$esc_radio_id = $data['esc_element_id'] . '-' . $option_value;
 
@@ -653,11 +653,11 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 					// Select.
 					case 'select':
 
-						if ( ! empty( $data['field']['options'] ) ) {
+						if (! empty( $data['field']['options'] )) {
 
 							$html .= '<select ' . $data['common_atts'] . ' id="' . $data['esc_element_id'] . '">';
 
-							foreach ( $data['field']['options'] as $option_value => $option_text ) {
+							foreach ($data['field']['options'] as $option_value => $option_text) {
 								$html .= '<option value="' . esc_attr( $option_value ) . '" ' . selected( $option_value, $data['value'], false ) . '> ' . esc_html( $option_text ) . '</option>';
 							}
 
@@ -680,7 +680,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 						$html = '';
 
-						if ( ! empty( $data['field']['content'] ) ) {
+						if (! empty( $data['field']['content'] )) {
 							$html .= wp_kses_post( $data['field']['content'] );
 						}
 
@@ -694,7 +694,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			$html = apply_filters( 'ctps_field_content_after', $html, $args );
 
 			// Add description beneath.
-			if ( ! empty( $data['field']['desc'] ) ) {
+			if (! empty( $data['field']['desc'] )) {
 				$html .= '<p class="description">';
 				$html .= wp_kses(
 					$data['field']['desc'],
@@ -743,12 +743,12 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			$output = array();
 
 			// Loop values.
-			foreach ( $input as $key => $value ) {
+			foreach ($input as $key => $value) {
 
 				// Invalid key.
 				// It is possible a plugin providing a setting has been deactivated.
 				// This avoids undefined index notice.
-				if ( ! isset( $this->fields[ $key ] ) ) {
+				if (! isset( $this->fields[ $key ] )) {
 					$value = '';
 				}
 
@@ -759,7 +759,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 					$value = trim( stripslashes( $value ) );
 
 					// Sanitize based on type.
-					switch ( $this->fields[ $key ]['type'] ) {
+					switch ($this->fields[ $key ]['type']) {
 
 						// Text.
 						// Textarea.
@@ -767,12 +767,12 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 						case 'textarea':
 
 							// Strip tags if config does not allow HTML.
-							if ( empty( $this->fields[ $key ]['allow_html'] ) ) {
+							if (empty( $this->fields[ $key ]['allow_html'] )) {
 								$value = trim( wp_strip_all_tags( $value ) );
 							}
 
 							// Sanitize HTML in case used (remove evil tags like script, iframe) - same as post content.
-							if ( ! current_user_can( 'unfiltered_html' ) ) { // admin only.
+							if (! current_user_can( 'unfiltered_html' )) { // admin only.
 								$value = stripslashes( wp_filter_post_kses( addslashes( $value ), $allowedposttags ) );
 							}
 
@@ -800,7 +800,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 						case 'select':
 
 							// If option invalid, stick with current value.
-							if ( ! isset( $this->fields[ $key ]['options'][$value] ) ) {
+							if (! isset( $this->fields[ $key ]['options'][$value] )) {
 								$value = $this->get( $key );
 							}
 
@@ -810,7 +810,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 						case 'number':
 
 							// If not a number, use default.
-							if ( ! is_numeric( $value ) ) {
+							if (! is_numeric( $value )) {
 								$value = $this->fields[ $key ]['default'];
 							}
 
@@ -827,7 +827,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 					}
 
 					// Run additional custom sanitization function if config requires it.
-					if ( ! empty( $this->fields[ $key ]['custom_sanitize'] ) ) {
+					if (! empty( $this->fields[ $key ]['custom_sanitize'] )) {
 						$value = call_user_func( $this->fields[ $key ]['custom_sanitize'], $value, $this->fields[ $key ] );
 					}
 
@@ -861,7 +861,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 
 			$is_settings_page = false;
 
-			if ( $this->page_hook_suffix === $screen->base ) {
+			if ($this->page_hook_suffix === $screen->base) {
 				$is_settings_page = true;
 			}
 
@@ -891,7 +891,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			$default = isset( $this->fields[ $setting ]['default'] ) ? $this->fields[ $setting ]['default'] : '';
 
 			// Setting not saved - use default value.
-			if ( ! isset( $settings[ $setting ] ) ) {
+			if (! isset( $settings[ $setting ] )) {
 				$value = $default;
 			}
 
@@ -899,7 +899,7 @@ if ( ! class_exists( 'CT_Plugin_Settings' ) ) { // in case class used in both th
 			else {
 
 				// Value is empty when not allowed, set default (no_empty true or is radio).
-				if ( empty( $settings[ $setting ] ) && ( ! empty( $this->fields[ $setting ]['no_empty'] ) || 'radio' === $this->fields[ $setting ]['type'] ) ) {
+				if (empty( $settings[ $setting ] ) && ( ! empty( $this->fields[ $setting ]['no_empty'] ) || 'radio' === $this->fields[ $setting ]['type'] )) {
 					$value = $default;
 				}
 
